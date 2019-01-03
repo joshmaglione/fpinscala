365116ae512b4c5f3da52c56386a5ebf8e3b9eb4
Josh Maglione <joshmaglione@gmail.com>
2019-01-03 20:32:19 +0100

    Refreshed and updated
    Re-forked the fpinscala repo, and loaded my old files into it. I didn't
    want to revert back, so I just started a new repo. Chapter 2 homework is
    completed, and some of chapter 3 is done as well.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -31,6 +31,8 @@ object List { // `List` companion object. Contains functions for creating and wo
     case _ => 101
   }
 
+  // Exercise 3.1: the value of x above is 3.
+
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
@@ -43,26 +45,63 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
+  /*
+    Exercise 3.8:
+      foldRight(List(1, 2, 3), Nil:List[Int])(Cons(_, _))
+   */
+
   def sum2(ns: List[Int]) =
     foldRight(ns, 0)((x,y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  /*
+    Exercise 3.7: product2 will not immediately halt if the list contains 0.0.
+    There is nothing in foldRight that performs a halt.
+   */
 
-  def tail[A](l: List[A]): List[A] = ???
+  // Exercise 3.2:
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => t
+    }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  // Exercise 3.3:
+  def setHead[A](l: List[A], h: A): List[A] =
+    Cons(h, l)
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  // Exercise 3.4:
+  def drop[A](l: List[A], n: Int): List[A] =
+    l match {
+      case Nil => Nil
+      case x if (n <= 0) => x
+      case Cons(h, t) => drop(t, n-1)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  // Exercise 3.5:
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case x => x
+    }
 
-  def init[A](l: List[A]): List[A] = ???
+  // Exercise 3.6: this cannot be in constant time because in order to remove
+  // the last link of a linked list, one must traverse the entire list!
+  def init[A](l: List[A]): List[A] =
+    l match{
+      case Nil => Nil
+      case Cons(x, Nil) => Nil
+      case Cons(x, Cons(y, t)) => init(Cons(y, t))
+    }
 
+  /*
   def length[A](l: List[A]): Int = ???
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  */
 }
